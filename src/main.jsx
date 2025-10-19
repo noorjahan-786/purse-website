1cf6d2dd6a010d6443b8c3be290700f0888965bc
noorjahan <noorjahan786.niet@gmail.com>
2025-10-19 10:57:17 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById('root')).render(
   <HashRouter>
-    <App />
+    <ShopContextProvider>
+    <App/>
+    </ShopContextProvider>
   </HashRouter>
-  
 )
-
