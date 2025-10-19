47b5bcc8d7fd74b0e456c82feba0ffb9c9821d56
noorjahan <noorjahan786.niet@gmail.com>
2025-10-19 10:35:06 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,7 @@ import ShopContextProvider from './context/ShopContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ShopContextProvider>
-
     <App/>
     </ShopContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
